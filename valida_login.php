6b7c8b8b67a511705ca8c7f88d7c1c631ffb88bb
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2023-01-17 15:27:38 -0300

    Feat: Verificando validação dos dados

--- valida_login.php
+++ valida_login.php
@@ -1,4 +1,25 @@
 <?php
+
+  $usuario_autenticado = false;
+
+  // Simulando banco de dados
+  $usuarios_app = array(
+    array('email' => 'adm@teste.com.br', 'senha' => '123456'),
+    array('email' => 'user@teste.com.br', 'senha' => 'abcd'),
+  );
+
+  foreach ($usuarios_app as $user) {
+    if ($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
+      $usuario_autenticado = true;
+    };
+  };
+
+  if ($usuario_autenticado) {
+    echo 'Usuário autenticado';
+  } else {
+    echo 'Erro na autenticação do usuário';
+  };
+
   // Pegando as informações pela super global GET e o nome é pelo name que foi informado la no HTML e são enviados como paramentros; 
   // Esta forma de recebimento é para o metodo GET
   /*
@@ -7,7 +28,9 @@
   */
 
   // Neste caso vamos enviar os dados com o Metodo POST
+  /*
   $_POST['email'];
   $_POST['senha'];
-
+  */
+  
 ?>
