cc1fab6e82d299498e22967213b56edd908c4376
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2023-01-17 15:13:28 -0300

    Feat: Formulario - Desenvolvendo os metodos get e post

---
+++ valida_login.php
@@ -0,0 +1,13 @@
+<?php
+  // Pegando as informações pela super global GET e o nome é pelo name que foi informado la no HTML e são enviados como paramentros; 
+  // Esta forma de recebimento é para o metodo GET
+  /*
+  $_GET['email'];
+  $_GET['senha'];
+  */
+
+  // Neste caso vamos enviar os dados com o Metodo POST
+  $_POST['email'];
+  $_POST['senha'];
+
+?>
