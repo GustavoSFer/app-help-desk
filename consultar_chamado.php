6d85b918132364c21a323dd0765c544ae4404881
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2023-01-19 12:56:21 -0300

    Feat: Consultando chamados

--- consultar_chamado.php
+++ consultar_chamado.php
@@ -1,5 +1,19 @@
 <? require_once "validador_acesso.php"; ?>
 
+<?php
+  $arquivo = fopen('arquivo.txt', 'r');
+  $chamados = array();
+
+  // feof => Retorna se é o final do arquivo;
+  while (!feof($arquivo)) {
+    // fgets => Retorna a informação da linha do arquivo;
+    $registro = fgets($arquivo);
+    $chamados[] = $registro;
+  }
+
+  fclose($arquivo);
+?>
+
 <html>
   <head>
     <meta charset="utf-8" />
@@ -40,24 +54,30 @@
             </div>
             
             <div class="card-body">
-              
-              <div class="card mb-3 bg-light">
-                <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
+              <?
+                foreach($chamados as $chamado){
+              ?>
 
+                <?
+                  $chamado_dados = explode('#', $chamado);
+                  /*
+                    chamado_dados é um array com 3 posições
+                    Neste caso estou verificando se o array é menor que 3 posição
+                    caso seja menor sabemos que tem um dados em branco e ele não será impresso.
+                  */
+                    if (count($chamado_dados) < 3) {
+                    continue;
+                  }
+                ?>
+                <div class="card mb-3 bg-light">
+                  <div class="card-body">
+                    <h5 class="card-title"><?=$chamado_dados[0]?></h5>
+                    <h6 class="card-subtitle mb-2 text-muted"><?=$chamado_dados[1]?></h6>
+                    <p class="card-text"><?=$chamado_dados[2]?></p>
+                  </div>
                 </div>
-              </div>
 
-              <div class="card mb-3 bg-light">
-                <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
-
-                </div>
-              </div>
+              <? } ?>
 
               <div class="row mt-5">
                 <div class="col-6">
